0cad257845b2061ce149d05d95fbceaf902a17d0
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2025-01-09 03:19:51 +0600

    localhost url is removd

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
