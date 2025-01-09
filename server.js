0229f8a633b5eab130f50b17b1136e26d8e4213d
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2025-01-09 18:25:35 +0600

    cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // app.use(cors());
 
 app.use(cors({
-  origin:  ['http://localhost:3000', 'https://expense-tracker-beta-kohl.vercel.app'],
+  origin:  ['http://localhost:3000', 'https://expense-tracker-beta-kohl.vercel.app', 'localhost/:1'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
