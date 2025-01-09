6206323449701bafef39c48dcf2547f860254946
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2025-01-09 18:45:50 +0600

    code refactored

--- server.js
+++ server.js
@@ -8,31 +8,6 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(cors());
-
-// app.use(cors({
-//   origin:  ['http://localhost:3000', 'https://expense-tracker-beta-kohl.vercel.app', 'localhost/:1'],
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-// }));
-
-
-// app.use(cors({
-//   origin: function(origin, callback) {
-//     // Allow requests with no origin (like mobile apps or curl requests)
-//     if (!origin) return callback(null, true);
-    
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-// }));
-
-
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 console.log('Environment:', process.env.NODE_ENV);
@@ -42,10 +17,6 @@ const allowedOrigins = [
   'https://expense-tracker-beta-kohl.vercel.app'
 ];
 
-app.use((req, res, next) => {
-  console.log('Incoming request from origin:', req.headers.origin);
-  next();
-});
 
 app.use(cors({
   origin: function(origin, callback) {
@@ -85,6 +56,12 @@ connectDB();
 app.use('/api/expenses', require('./routes/expenses'));
 app.use('/api/limits', require('./routes/limits'));
 
+app.use((req, res, next) => {
+  console.log('Incoming request from origin:', req.headers.origin);
+  next();
+});
+
+
 app.use('/', (req, res) => {
   res.send(`Server running on the ${PORT}`)
 })
@@ -94,3 +71,5 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
